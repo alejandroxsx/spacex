d1426b297ceb1a185cb7776ce3a41583e37913db
Alejandro Rodriguez <alejandro.xsc@gmail.com>
2020-07-07 13:26:34 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- copyright -->
+				<div class="wrapper">
+					<p>Space X &copy; 2020</p>
+					<p>TWITTER</p>
+					<p>YOUTUBE</p>
+					<p>INSTAGRAM</p>
+					<p>FLICKR</p>
+					<p>LINKEDIN</p>
+					<p>PRIVACY POLICY</p>
+				</div>
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
